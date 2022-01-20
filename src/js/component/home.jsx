6bb1676768075c73078485b49751e2bba562c00a
etomedina <hmedina@LAPTOP-84M4TVEB.localdomain>
2022-01-19 22:29:01 -0400

    Modificacion home.jsx index.js y index.css

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,28 +1,30 @@
 import React from "react";
+import propTypes from "prop-types";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
+function Home(props) {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">
-					4Geeks Academy Prueba 1
-				</a>
-				, with love!
-			</p>
-		</div>
+		<>
+			<div className="MainCounter">
+				<div className="Imagen">
+					<i className="far fa-clock"></i>
+				</div>
+				<div className="four">{props.digFour}</div>
+				<div className="thre">{props.digThree}</div>
+				<div className="two">{props.digTwo}</div>
+				<div className="one">{props.digOne}</div>
+			</div>
+		</>
 	);
+}
+
+Home.propTypes = {
+	digFour: propTypes.number,
+	digThree: propTypes.number,
+	digTwo: propTypes.number,
+	digOne: propTypes.number,
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,19 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let counter = 0;
+setInterval(function () {
+	const four = Math.floor(counter / 1000) % 10;
+	const three = Math.floor(counter / 100) % 10;
+	const two = Math.floor(counter / 10) % 10;
+	const one = Math.floor(counter / 1) % 10;
+	counter++;
+
+	//render your react application
+	ReactDOM.render(
+		<Home digOne={one} digTwo={two} digThree={three} digFour={four} />,
+		document.querySelector("#app")
+	);
+}, 1000);
